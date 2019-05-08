0b305e393ddc265becb0dba4bf6262c5b83734b5
Teacher <invalid@domain.com>
2019-05-08 16:18:13 +0200

    Changed code

--- TestWeb/src/dk/test/TestServlet2.java
+++ TestWeb/src/dk/test/TestServlet2.java
@@ -34,6 +34,7 @@ public class TestServlet2 extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().write("Hello World");
 	}
 
 }
