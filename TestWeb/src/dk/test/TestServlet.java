d208084f85f9d2af3bfa12d17e89e5ae4b33ef3c
extadb <extadb@DK000R90S929B.semlernet.dk>
2019-05-10 17:06:11 +0200

    Modified again

--- TestWeb/src/dk/test/TestServlet.java
+++ TestWeb/src/dk/test/TestServlet.java
@@ -37,7 +37,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Adil+KW");
+		System.out.println("Adil+KW+more");
 	}
 
 }
