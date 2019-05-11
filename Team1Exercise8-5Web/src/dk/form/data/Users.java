3d85174c4fdb99f38c71dd02b27216500631466d
JavaToGo <JavaToGo@DKxxxPC09PMAZ>
2019-05-11 13:27:39 +0200

    User Class

--- Team1Exercise8-5Web/src/dk/form/data/Users.java
+++ Team1Exercise8-5Web/src/dk/form/data/Users.java
@@ -1,5 +1,28 @@
 package dk.form.data;
 
 public class Users {
-
+	private String email = null;
+	private String name = null;
+	private String age = null;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
 }
