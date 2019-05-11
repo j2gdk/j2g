1dab54ef9b90753e429e4877bb275ab972873049
JavaToGo <JavaToGo@DKxxxPC09PMAZ>
2019-05-11 13:37:07 +0200

    Output.java

--- Team1Exercise8-5Web/src/dk/form/servlet/Form.java
+++ Team1Exercise8-5Web/src/dk/form/servlet/Form.java
@@ -26,14 +26,14 @@ public class Form extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 	}
 
 }
--- Team1Exercise8-5Web/src/dk/form/servlet/Output.java
+++ Team1Exercise8-5Web/src/dk/form/servlet/Output.java
@@ -26,14 +26,14 @@ public class Output extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.getRequestDispatcher("output.jsp").forward(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.sendRedirect("output");
 	}
 
 }
