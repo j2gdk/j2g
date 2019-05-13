0aad6999332777b14a7485649e5ee07b3e135ea5
Teacher <invalid@domain.com>
2019-05-13 18:34:24 +0200

    manual merge

--- GitExercise13-5/src/dk/javatogo/servlet/TestServlet.java
+++ GitExercise13-5/src/dk/javatogo/servlet/TestServlet.java
@@ -26,7 +26,7 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().write("This is doGet - new");
+		response.getWriter().write("This is doGet - new - pc1");
 	}
 
 	/**
