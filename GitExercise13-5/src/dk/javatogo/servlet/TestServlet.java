0068852cd028da0b981037678c26cece7502bdfc
Teacher <invalid@domain.com>
2019-05-13 18:14:43 +0200

    New exercise

--- GitExercise13-5/src/dk/javatogo/servlet/TestServlet.java
+++ GitExercise13-5/src/dk/javatogo/servlet/TestServlet.java
@@ -1,7 +1,6 @@
-package dk.javatogo.gitexercise;
+package dk.javatogo.servlet;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,8 +26,14 @@ public class TestServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String model = request.getParameter("model");
-		
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 
 }
