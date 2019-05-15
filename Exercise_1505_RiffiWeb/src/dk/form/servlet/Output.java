59da9fc39bcb2522a53367bdc583c327f2d31819
RW <JavaToGo@DKxxxPC05475R>
2019-05-15 18:50:12 +0200

    copied code

---
+++ Exercise_1505_RiffiWeb/src/dk/form/servlet/Output.java
@@ -0,0 +1,48 @@
+package dk.form.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Output
+ */
+@WebServlet("/Output")
+public class Output extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Output() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("output.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getParameter("previous") != null) {
+			response.sendRedirect("Form");
+
+		} else {
+
+			response.sendRedirect("Output");
+		}
+		
+		
+	}
+	
+
+}
