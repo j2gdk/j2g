9c2d03c5e9585116dc4ef678156b5ff8f90fa003
JavaToGo <JavaToGo@DKxxxPC0CJ37J>
2019-05-22 14:34:55 +0200

    Update servlet created

--- Exercise_1505_AfzalWeb/src/dk/exercise_1505_AfzalWeb/servlet/Update.java
+++ Exercise_1505_AfzalWeb/src/dk/exercise_1505_AfzalWeb/servlet/Update.java
@@ -26,7 +26,6 @@ public class Update extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	/**
