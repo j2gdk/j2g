c8e3116eb819521a0564720455daa225dc797913
JavaToGo <JavaToGo@192.168.0.30>
2019-06-03 09:35:08 +0200

    working on it

--- Exercise_2205_AwesAdilWeb/src/dk/cars/data/validator.java
+++ Exercise_2205_AwesAdilWeb/src/dk/cars/data/validator.java
@@ -1,5 +1,28 @@
 package dk.cars.data;
 
+
 public class validator {
-	public int 
-}
+	 int private myNum = null; 
+	
+	int min = 1; 
+	int max = 10; 
+
+	boolean valid = true; 
+	
+	if (0 < 1); {
+	}	
+
+		return valid; 
+		
+	
+	
+	
+			
+
+	            
+		}
+		
+}	
+	
+	
+
