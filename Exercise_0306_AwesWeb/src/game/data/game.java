9fac2cf78936525aa35dd7832d87a80441291a61
JavaToGo <JavaToGo@192.168.0.30>
2019-06-13 14:07:37 +0200

    Not working yet

---
+++ Exercise_0306_AwesWeb/src/game/data/game.java
@@ -0,0 +1,112 @@
+package game.data;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class game
+ */
+@WebServlet("/game")
+public class game extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Integer EMPTY = null;
+	private static final String X = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public game() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession s=request.getSession();
+		
+		ArrayList<Integer> game = new ArrayList<Integer>();
+		game.add(0, EMPTY);
+		game.add(1, EMPTY);
+		game.add(2, EMPTY);
+		game.add(3, EMPTY);
+		game.add(4, EMPTY);
+		game.add(5, EMPTY);
+		game.add(6, EMPTY);
+		game.add(7, EMPTY);
+		game.add(8, EMPTY);
+		
+		
+		s.setAttribute("gameList",game);
+		String nextJSP = "/game.jsp";
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP); 
+		
+		dispatcher.forward(request,response);
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession s=request.getSession();
+		ArrayList<Integer> gameList = null;
+		
+		request.getSession().getAttribute("gameList"); 
+		
+		String box = request.getParameter("box");
+		game.remove(Integer.parseInt(box));
+		game.add(Integer.parseInt(box), X);
+		
+		if (isgameFull(gameList)); { 
+			request.setAttribute("game over", "try again");
+			
+		} else { 
+		
+// https://www.tutorialspoint.com/java/java_break_statement.htm
+			
+			game.firstElement();
+			for (int game : gameList) {
+		         if (game == EMPTY) {
+		            break;
+			
+		         }
+			}           
+		            
+		
+		if (XWon(gameList)) { 
+			request.setAttribute("You won the game", box); 
+			
+		} else if {
+			
+		(OWon(gameList)) { 
+			request.setAttribute("You lost the game");
+		}
+		
+		request.getRequestDispatcher("game.jsp").forward(request, response);
+		
+		}
+		
+		private boolean isgameFull(ArrayList<Integer> board) {
+			for (Integer box : gameList) {
+				if (box == EMPTY) {
+					return false;
+				
+	}
+		
+				
+		
+		
+
+}
---
+++ Exercise_0306_AwesWeb/src/game/data/gameclass.java
@@ -0,0 +1,10 @@
+package game.data;
+
+public class gameclass {
+	
+	public static final Integer X = 0; 
+	public static final Integer O = 1; 
+	public static final Integer EMPTY = 2; 
+	
+
+}
